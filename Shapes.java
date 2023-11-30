ff9c1c4a30b6eb910918073c0b1a1afcd6016ae4
Saad <saadamawi@yahoo.com>
2023-11-30 22:09:49 +0400

    testing

---
+++ Shapes.java
@@ -0,0 +1,6 @@
+
+abstract class Shapes{
+
+    void draw(){}
+
+}
---
+++ main.java
@@ -0,0 +1,19 @@
+/*
+ * Sudent ID: 1077321
+ * Name: Saad Amawi
+ */
+
+public class main extends Shapes{
+
+    public static void main(String[]args){
+
+       Shapes circleObj = new Circle();
+       Shapes squareObj = new Square();
+       Shapes triangleObj = new Triangle();
+
+        circleObj.draw();
+        squareObj.draw();
+        triangleObj.draw();
+
+    }
+}
