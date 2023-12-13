668c66dfe34c495fc08b8d2318290119c28da51e
Sovereign2 <saadamawi@yahoo.com>
2023-12-14 00:32:23 +0400

    ENOUGH

--- reactjs/src/Components/Footer.js
+++ reactjs/src/Components/Footer.js
@@ -24,40 +24,34 @@ function Footer() {
           <div class='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+            <Link to=''>Testimonials</Link>
+            <Link to=''>Careers</Link>
+            <Link to=''>Investors</Link>
+            <Link to=''>Terms of Service</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <Link to=''>Contact</Link>
+            <Link to=''>Support</Link>
+            <Link to=''>Destinations</Link>
+            <Link to=''>Sponsorships</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <Link to=''>Submit Video</Link>
+            <Link to=''>Ambassadors</Link>
+            <Link to=''>Agency</Link>
+            <Link to=''>Influencer</Link>
           </div>
-          <div class='footer-link-items2'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
-          </div>
+     
         </div>
       </div>
       <section class='social-media'>
         <div class='social-media-wrap'>
           <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
+            <Link to='' className='social-logo'>
             <i class="fa-regular fa-eye"/> &nbsp; MemorEyez 
               
             </Link>
@@ -65,41 +59,36 @@ function Footer() {
           <small class='website-rights'>MemorEyez © 2023</small>
           <div class='social-icons'>
             <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
+              class='social-icon-link-blue'
+              to=''
               aria-label='Facebook'
             >
               <i class='fab fa-facebook-f' />
             </Link>
             <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
+              class='social-icon-link-insta'
+              to=''
               aria-label='Instagram'
             >
               <i class='fab fa-instagram' />
             </Link>
             <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
+              class='social-icon-link-youtube'
+              to=''
               aria-label='Youtube'
             >
               <i class='fab fa-youtube' />
             </Link>
             <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
+              class='social-icon-link-twitter'
+              to=''
               aria-label='Twitter'
             >
               <i class='fab fa-twitter' />
             </Link>
             <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
+              class='social-icon-link-blue'
+              to=''
               aria-label='LinkedIn'
             >
               <i class='fab fa-linkedin' />
